ddfaecbc59c33136723021aeaa940ae8fb0e1b1a
Innocent <rupiahinnocent66@gmail.com>
2022-08-26 16:48:51 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['cloudflare-ipfs.com', 'localhost'],
+  },
 }
 
 module.exports = nextConfig
